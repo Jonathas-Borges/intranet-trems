134ee5ed3192fd1e7f1aec0164b3b753036fb266
Jonathas-Borges <jonathasborges12@gmail.com>
2024-05-22 16:03:35 -0400

    Exercício 24: Remove duplicação de código de validadores

--- backend/src/intranet_trems/src/intranet_trems/content/area.py
+++ backend/src/intranet_trems/src/intranet_trems/content/area.py
@@ -1,4 +1,8 @@
+from intranet_trems import _
+from intranet_trems import validadores
 from plone.dexterity.content import Container
+from plone.schema.email import Email
+from plone.supermodel import model
 from plone.supermodel.model import Schema
 from zope import schema
 from zope.interface import implementer
@@ -7,13 +11,27 @@ from zope.interface import implementer
 class IArea(Schema):
     """Definição de uma Área no TRE-MS."""
 
-    # Informações básicas
-    title = schema.TextLine(title="Nome da Área", required=True)
-    description = schema.Text(title="Descrição", required=False)
+    title = schema.TextLine(title=_("Nome da Área"), required=True)
+    description = schema.Text(title=_("Descrição"), required=False)
 
-    # Contato
-    email = schema.TextLine(title="E-mail", required=True)
-    ramal = schema.TextLine(title="Ramal", required=True)
+    model.fieldset(
+        "contato",
+        _("Contato"),
+        fields=[
+            "email",
+            "ramal",
+        ],
+    )
+    email = Email(
+        title=_("Email"),
+        required=True,
+        constraint=validadores.is_valid_email,
+    )
+    ramal = schema.TextLine(
+        title=("Ramal"),
+        required=True,
+        constraint=validadores.is_valid_extension,
+    )
 
 
 @implementer(IArea)
