a5293e1177a9d58bcb084a28f81a73fd4d39d564
Jonathas-Borges <jonathasborges12@gmail.com>
2024-05-22 16:04:29 -0400

    Validadores e upgrade de versão

---
+++ backend/src/intranet_trems/src/intranet_trems/upgrades/v20240522002.py
@@ -0,0 +1,28 @@
+from intranet_trems import logger
+from intranet_trems.setuphandlers.content import populate_portal
+from plone import api
+
+
+def cria_estrutura(context):
+    portal = api.portal.get()
+    populate_portal(
+        portal,
+        [
+            "TRE-MS",
+        ],
+    )
+    logger.info("Criando testes de perssionamento para Área")
+
+
+def alterar_permissionamento_estrutura(context):
+    portal = api.portal.get()
+    estrutura = portal["estrutura"]
+    permission_id = "intranet_trems: Add Area"
+    roles = [
+        "Contributor",
+        "Editor",
+        "Manager",
+        "Site Administrator",
+    ]
+    estrutura.manage_permission(permission_id, roles=roles)
+    logger.info(f"Alterado permissionamento em {estrutura}.")
