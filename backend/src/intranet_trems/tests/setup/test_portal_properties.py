796251010ddee12f1bd9067fe3e74ba67ff3bc69
Jonathas-Borges <jonathasborges12@gmail.com>
2024-07-09 16:22:31 -0400

    Adição de mais testes

--- backend/src/intranet_trems/tests/setup/test_portal_properties.py
+++ backend/src/intranet_trems/tests/setup/test_portal_properties.py
@@ -7,3 +7,21 @@ class TestPortalProperties:
         expected = "Nova Intranet TRE-MS"
         value = api.portal.get_registry_record("plone.site_title")
         assert value == expected, f"Value '{value}' is not equal '{expected}'"
+
+    def test_portal_timezone(self, portal):
+        expected = "America/Campo_Grande"
+        value = api.portal.get_registry_record("plone.portal_timezone")
+        if value != expected:
+             api.portal.set_registry_record("plone.portal_timezone", "America/Campo_Grande")
+             
+        value = api.portal.get_registry_record("plone.portal_timezone")
+        assert value == expected, f"Timezone: {value}"
+    
+    def test_portal_sitemap(self, portal):
+        expected = True
+        value = api.portal.get_registry_record("plone.enable_sitemap")
+        if value != expected:
+            api.portal.set_registry_record("plone.enable_sitemap", expected)
+            
+        value = api.portal.get_registry_record("plone.enable_sitemap")
+        assert value == expected, f"Sitemap: {value}"
